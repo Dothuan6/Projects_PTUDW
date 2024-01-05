b31d0c42f614c1d069e67c9e50500e5165a703f6
Dothuan6 <thuann6222@gmail.com>
2024-01-06 00:14:34 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const contactsRouter = require("./app/routes/contact.route");
 const express = require("express");
 const cors = require("cors");
+//xu ly loi
+const ApiError = require('./app/api-error');
 
 const app = express();
+app.use("/api/contacts",contactsRouter);
 
-app.use("/api/contacts" , contactsRouter);
+app.use((reg,res,next)=>{
+    return next(new ApiError(404,'Resource not found'));
+});
+
+app.use((reg,res,next)=>{
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error"
+    });
+});
+
+
 app.use(cors());
 app.use(express.json());
 app.get("/home", (reg,res) => {
